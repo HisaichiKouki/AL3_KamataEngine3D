465cfd8197d17914afba46aef30a374f3879fd2a
ヒサイチ_コウキ <k023g0068@g.neec.ac.jp>
2024-04-15 14:59:36 +0900

    Playerクラスで描画

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -4,16 +4,27 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete model_;
+	delete player_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	texHandle_ = TextureManager::Load("uvChecker.png");
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+
+	player_ = new Player();
+	player_->Initialize(model_,texHandle_);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	player_->Update(); }
 
 void GameScene::Draw() {
 
@@ -41,7 +52,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
+	player_->Draw(viewProjection_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
